fb4712599f79d8156c83057e0833d2b3e443b664
tripleseven190504 <tripleseven190504@gmail.com>
2024-05-29 22:03:43 +0700

    add sln to project

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import './index.css';
+import Login from './pages/Login';
+const routes = createRoutesFromElements(
+  <>
+    <Route path="/" element={<Login />} />
+    <Route path="/home" element={<Login />}>
+      <Route index element={<Navigate to="/home/welcome" replace />} />
+      <Route path="student" element={<Login />} />
+    </Route>
+  </>,
+);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ react/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+const Login = () => {
+  return (
+    <>
+      <h1>Xin chao</h1>
+    </>
+  );
+};
+export default Login;
