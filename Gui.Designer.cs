d3585a0eceb474f63537e3296fa15abd208d6a40
tripleseven190504 <tripleseven190504@gmail.com>
2024-05-29 19:12:21 +0700

    init

---
+++ Gui.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace THPTAdminSystem
+{
+    partial class Gui
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gui));
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView2.Location = new System.Drawing.Point(0, 0);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(984, 461);
+            this.webView2.TabIndex = 0;
+            this.webView2.ZoomFactor = 1D;
+            // 
+            // Gui
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.webView2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Gui";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "THPT Admin System";
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+    }
+}
+
---
+++ react/vite.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('vite').UserConfig} */
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		host: "0.0.0.0",
+	},
+	build: {
+		emptyOutDir: true,
+		outDir: "../src",
+	},
+});
